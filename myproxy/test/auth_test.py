379d26e24102bbe7bde50e13906cd7c4c990aa23
Stephcyrille <stephcyril.sc@gmail.com>
2024-09-06 19:27:23 +0200

    Update test case for auth

--- myproxy/test/auth_test.py
+++ myproxy/test/auth_test.py
@@ -1,12 +1,8 @@
 from django.test import TestCase
-from myproxy.utils import connect_to_odoo
+from myproxy.utils.connector import APIConnector
 
-class AuthOdooTestCase(TestCase):
+class AuthConnectorTestCase(TestCase):
     def setUp(self) -> None:
-        self.driverId = 1
-        self.constructorId = 131
-        self.circuitId = 3
-        self.race_round = 1
-        self.year = 2023
+        pass
 
     
